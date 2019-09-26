6a1d42f6329b782e5a4e6ead8d3ea5fc8dd6a044
Maeve Griffin <griffinmaeve@gmail.com>
2019-09-26 15:44:03 -0700

    created first routes

---
+++ e2e/__tests__/actors.test.js
@@ -0,0 +1,32 @@
+const request = require('../request');
+const db = require('../db');
+
+
+describe.only('actors api', () => {
+  beforeEach(() => {
+    return Promise.all([
+      db.dropCollection('actors')
+    ]);
+  });
+  const john = {
+    name: 'John Travolta',
+    birthYear: 1953
+  };
+
+  function postActor(john) {
+    return request 
+      .post('/api/actors')
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('posts an actor', () => {
+    return postActor(john).then(actor => {
+      expect(actor).toMatchInLineSnapshot(
+        {
+          _id: expect.any(String)
+        },
+      );
+    });
+  });
+});
