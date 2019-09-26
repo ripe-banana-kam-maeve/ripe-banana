4027cedce78b8247a8b595a4113da5bda833c351
Kamran Ali <kgali21@gmail.com>
2019-09-26 16:07:13 -0700

    made get and post routes and test them positive

--- e2e/__tests__/actors.test.js
+++ e2e/__tests__/actors.test.js
@@ -1,32 +1,59 @@
 const request = require('../request');
 const db = require('../db');
 
-
-describe.only('actors api', () => {
+describe('studios', () => {
   beforeEach(() => {
-    return Promise.all([
-      db.dropCollection('actors')
-    ]);
+    return db.dropCollection('actors');
   });
+
   const john = {
     name: 'John Travolta',
     birthYear: 1953
   };
 
   function postActor(john) {
-    return request 
+    return request
       .post('/api/actors')
+      .send(john)
       .expect(200)
       .then(({ body }) => body);
   }
 
   it('posts an actor', () => {
     return postActor(john).then(actor => {
-      expect(actor).toMatchInLineSnapshot(
-        {
-          _id: expect.any(String)
-        },
-      );
+      expect(actor).toEqual({
+        _id: expect.any(String),
+        __v: 0,
+        ...john
+      });
+    });
+  });
+
+  it('gets an actor by id', () => {
+    return postActor(john).then(actor => {
+      return request
+        .post('/api/actors')
+        .send(john)
+        .expect(200)
+        .then(() => {
+          return request.get(`/api/actors/${actor._id}`).expect(200);
+        })
+        .then(({ body }) => {
+          expect(body).toMatchInlineSnapshot(
+            {
+              _id: expect.any(String)
+            },
+
+            `
+            Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "birthYear": 1953,
+              "name": "John Travolta",
+            }
+          `
+          );
+        });
     });
   });
 });
