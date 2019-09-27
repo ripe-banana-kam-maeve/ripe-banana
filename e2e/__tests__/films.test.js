05b3d1bef9fe345003441ec10c561c5b4bc24689
Kamran Ali <kgali21@gmail.com>
2019-09-27 13:54:14 -0700

    fixing paths, and finding bug post errors

---
+++ e2e/__tests__/films.test.js
@@ -0,0 +1,80 @@
+const request = require('../request');
+const db = require('../db');
+
+describe.only('films api', () => {
+  beforeEach(() => {
+    return Promise.all([
+      db.dropCollection('films'),
+      db.dropCollection('actors'),
+      db.dropCollection('studios')
+    ]);
+  });
+
+
+
+  const tom = {
+    name: 'Tom Hanks',
+    birthYear: 1953
+  };
+
+  function postActor(tom) {
+    return request
+      .post('/api/actors')
+      .send(tom)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  const disney = {
+    name: 'Pixar',
+    yearFounded: 1925,
+    alive: true
+  };
+
+
+  function postStudio(disney) {
+    return request
+      .post('/api/studios')
+      .send(disney)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  const toyStory = {
+    title: 'toy story',
+    director: 'tom hanks',
+    yearPub: 1991,
+    digital: false,
+    actors: [],
+    studio: {},
+  };
+
+  function postFilm(film) {
+    return Promise.all([
+      postActor(tom),
+      postStudio(disney)
+    ])
+      .then(([actor, studio]) => {
+        film.actors[0] = actor._id;
+        film.studio = studio._id;
+        return request
+          .post('/api/films')
+          .send(toyStory)
+          .expect(200);
+      })
+      .then(({ body }) => body);
+  }
+
+  it('posts a film', () => {
+    return postFilm(toyStory).then(film => {
+      expect(film).toMatchInlineSnapshot(
+        {
+          _id: expect.any(String),
+          actor: [expect.any(String)],
+          studio: expect.any(String)
+        },
+      );
+    });
+  });
+
+});
