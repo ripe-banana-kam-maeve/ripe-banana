7ca2d61772dc2088c72adf29c37de7b41c69bce5
Kamran Ali <kgali21@gmail.com>
2019-09-26 17:03:40 -0700

    get, put, post for reviewers testing correctly

--- e2e/__tests__/reviewers.test.js
+++ e2e/__tests__/reviewers.test.js
@@ -29,4 +29,78 @@ describe('reviewer api', () => {
       });
     });
   });
+
+  it('get reviewer by id', () => {
+    return postReviewer(fakeGuy).then(reviewers => {
+      return request
+        .post('/api/reviewers')
+        .send(fakeGuy)
+        .expect(200)
+        .then(() => {
+          return request.get(`/api/reviewers/${reviewers._id}`).expect(200);
+        })
+        .then(({ body }) => {
+          expect(body).toMatchInlineSnapshot(
+            {
+              _id: expect.any(String)
+            },
+
+            `
+            Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "name": "bob",
+              "print": true,
+              "publication": "nytimes",
+              "yearsPub": 4,
+            }
+          `
+          );
+        });
+    });
+  });
+
+  it('get all reviewers', () => {
+    return postReviewer(fakeGuy).then(() => {
+      return request
+        .post('/api/reviewers')
+        .send(fakeGuy)
+        .expect(200)
+        .then(() => {
+          return request.get(`/api/reviewers`).expect(200);
+        })
+        .then(({ body }) => {
+          expect(body[0]).toMatchInlineSnapshot(
+            {
+              _id: expect.any(String)
+            },
+
+            `
+            Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "name": "bob",
+              "print": true,
+              "publication": "nytimes",
+              "yearsPub": 4,
+            }
+          `
+          );
+        });
+    });
+  });
+
+  it('updates a reviewer', () => {
+    return postReviewer(fakeGuy)
+      .then(reviewer => {
+        reviewer.publication = 'fakestuff';
+        return request
+          .put(`/api/reviewers/${reviewer._id}`)
+          .send(reviewer)
+          .expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.publication).toBe('fakestuff');
+      });
+  });
 });
