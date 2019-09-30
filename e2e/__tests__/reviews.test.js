1f67604cb747e43edb709355f34035f5862a9d77
Maeve Griffin <griffinmaeve@gmail.com>
2019-09-30 15:01:29 -0700

    working on review post test

--- e2e/__tests__/reviews.test.js
+++ e2e/__tests__/reviews.test.js
@@ -28,24 +28,41 @@ describe('reviews', () => {
     ]
   };
 
-  const testReview = {
+  const testReview1 = {
     rating: 4,
     reviewer: expect.any(String),
     review: 'was bad',
     film: expect.any(String)
   };
+  const testReview2 = {
+    rating: 5,
+    reviewer: expect.any(String),
+    review: 'was bad',
+    film: expect.any(String)
+  };
+  const testReview3 = {
+    rating: 6,
+    reviewer: expect.any(String),
+    review: 'was bad',
+    film: expect.any(String)
+  };
+  const testReview4 = {
+    rating: 7,
+    reviewer: expect.any(String),
+    review: 'was bad',
+    film: expect.any(String)
+  };
 
-  function postReview(testReview) {
+  function postReview(testReview1) {
     return Promise.all([
       postReviewer(fakeGuy), postFilm(toyStory)
     ])
       .then(([reviewer, film]) => {
-        testReview.film = film;
-        testReview.reviewer = reviewer;
-        console.log('fasdf', testReview);
+        testReview1.film = film;
+        testReview1.reviewer = reviewer;
         return request
           .post('/api/reviews')
-          .send(testReview)
+          .send(testReview1)
           .expect(200);
       })
       .then(({ body }) => {
@@ -53,11 +70,34 @@ describe('reviews', () => {
       });
   }
   it('posts an review', () => {
-    return postReview(testReview).then(review => {
-      console.log(review);
+    return postReview(testReview1).then(review => {
       expect(review).toEqual(
         { '__v': 0, '_id': expect.any(String), 'film': expect.any(String), 'rating': 4, 'review': 'was bad', 'reviewer': expect.any(String) }
       );
     });
+  });
+  it('gets reviews', () => {
+    return Promise.all([
+      postReview(testReview1),
+      postReview(testReview2),
+      postReview(testReview3),
+      postReview(testReview4)
+    ])
+      .then(([r1, r2, r3, r4]) => {
+        return request
+          .post(`/api/reviews`).send(r1).expect(200)
+          .post(`/api/reviews`).send(r2).expect(200)
+          .post(`/api/reviews`).send(r3).expect(200)
+          .post(`/api/reviews`).send(r4).expect(200);
+      })
+      .then(({ body }) => {
+        console.log('body', body);
+        expect(body[0]).toMatchInlineSnapshot(
+          {
+            _id: expect.any(String)
+          },
+
+        );
+      });
   });
 });
