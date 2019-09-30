6164c27a13e3d88b52e99e5101ebd266f469dfa9
Kamran Ali <kgali21@gmail.com>
2019-09-30 14:38:47 -0700

    passing all tests so far

--- e2e/__tests__/reviews.test.js
+++ e2e/__tests__/reviews.test.js
@@ -1,6 +1,6 @@
-//const request = require('../request');
+const request = require('../request');
 const db = require('../db');
-//const { postFilm } = require('../../e2e/__tests__/films.test');
+const { postFilm } = require('../../e2e/__tests__/films.test');
 const { postReviewer } = require('../__tests__/reviewers.test');
 
 describe('reviews', () => {
@@ -15,42 +15,49 @@ describe('reviews', () => {
     name: 'Dwane the Rock Johnson',
     company: 'School of Hard Knocks'
   };
-  // const toyStory = {
-  //   title: 'Alladin',
-  //   studio: { _id: expect.any(String), name: expect.any(String) },
-  //   released: 1977,
-  //   cast: [
-  //     {
-  //       _id: expect.any(String),
-  //       role: 'champ',
-  //       actor: { _id: expect.any(String), name: expect.any(String) }
-  //     }
-  //   ]
-  // };
+  const toyStory = {
+    title: 'Alladin',
+    studio: { _id: expect.any(String), name: expect.any(String) },
+    released: 1977,
+    cast: [
+      {
+        _id: expect.any(String),
+        role: 'champ',
+        actor: { _id: expect.any(String), name: expect.any(String) }
+      }
+    ]
+  };
 
-  const data = {
+  const testReview = {
     rating: 4,
     reviewer: expect.any(String),
     review: 'was bad',
     film: expect.any(String)
   };
 
-  function postReview(data) {
-    return postReviewer(fakeGuy).then(reviewer => {
-      console.log(data.reviewer);
-      data.reviewer = reviewer;
-    })
-      .post('/api/reviews')
-      .send(data)
-      .expect(200)
+  function postReview(testReview) {
+    return Promise.all([
+      postReviewer(fakeGuy), postFilm(toyStory)
+    ])
+      .then(([reviewer, film]) => {
+        testReview.film = film;
+        testReview.reviewer = reviewer;
+        console.log('fasdf', testReview);
+        return request
+          .post('/api/reviews')
+          .send(testReview)
+          .expect(200);
+      })
       .then(({ body }) => {
         return body;
       });
   }
   it('posts an review', () => {
-    return postReview(data).then(review => {
+    return postReview(testReview).then(review => {
       console.log(review);
-      expect(review).toEqual(6);
+      expect(review).toEqual(
+        { '__v': 0, '_id': expect.any(String), 'film': expect.any(String), 'rating': 4, 'review': 'was bad', 'reviewer': expect.any(String) }
+      );
     });
   });
 });
